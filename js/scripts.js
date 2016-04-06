628c62d6f1da3ec9cfff390f31253003940049cf
Gigi Ditz and Inthrayuth Mahaphol <inthrayuth@>
2016-04-06 14:54:37 -0700

    create Reset Board function in script

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
 // business logic
+var winArray = ["XXX", "OOO"];
 
 
+
+  // function to clear board ---------------
+function resetBoard() {
+    $("#a1, #a2, #a3, #b1, #b2, #b3, #c1, #c2, #c3").empty("");
+}
 
 // user interface logic
-$(document).ready(function(){
+$(document).ready(function() {
 
+  var mark = "X"
+
+
+  $("#a1").click(function() {
+    $("#a1").text(mark);
+  });
+  $("#a2").click(function() {
+    $("#a2").text(mark);
+  });
+
+  $("#newGame").click(function(){
+    resetBoard();
+  });
 });
