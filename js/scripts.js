728a6b39fc522d43e385df1d63c8df0d2b359255
Gigi Ditz and Inthrayuth Mahaphol <gigi@>
2016-04-06 16:28:37 -0700

    modified html and added loop in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,69 @@
 // business logic
 var winArray = ["XXX", "OOO"];
 
+  // function to check empty space --------------
+var emptySpace = function(position) {
+  if (position === "") {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
+  // function to check mark is X or O --------------
+var checkMark = function(player, mark) {
+  if (player === 1) {
+    mark = "X";
+  }
+  else {
+    player = 2;
+    mark = "O";
+  }
+}
+
+// var boardNotFull = function(allPosition) {
+//   allPosition.map(function(i) {
+//     if (i === "") {
+//       return true;
+//     }
+//     else {
+//       return false;
+//     }
+//   });
+// }
 
   // function to clear board ---------------
 function resetBoard() {
-    $("#a1, #a2, #a3, #b1, #b2, #b3, #c1, #c2, #c3").empty("");
+    $("td").empty("");
 }
 
 // user interface logic
 $(document).ready(function() {
+  var player = 1;
+  var mark = "X";
+  var notWinner = true;
 
-  var mark = "X"
+  for (; notWinner;) {
+    $("#a1").click(function() {
+      if (emptySpace) {
+        $("#a1").text(mark);
+      }
+    });
+
+    notWinner = false;  // this line is for temporary stop the loop. it will be deleted soon.
+
+// this step need to check the winner
 
 
-  $("#a1").click(function() {
-    $("#a1").text(mark);
-  });
-  $("#a2").click(function() {
-    $("#a2").text(mark);
-  });
+// this step is to check the mark character before go to the next loop
+    if (mark === "X") {
+      mark = "O";
+    }
+    else (mark === "O") {
+      mark = "X";
+    }
+  }  // end loop section
 
   $("#newGame").click(function(){
     resetBoard();
